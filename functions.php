8d48e3f37420122faa5e938c5f604bcdba63089b
DESKTOP-1S608KI\WINDOWS <mittapabt2536@gmail.com>
2023-02-27 15:16:29 +0600

    update

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function link_bootstrap(){
     wp_enqueue_style( "link_bootstrap", '//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', array() , '1.0', '' );
+    wp_enqueue_script('link_bt_sc', '//cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',  array(), '1.0', true);
 }
 
 add_action('wp_enqueue_scripts','link_bootstrap');
@@ -36,3 +37,31 @@ function create_shortcode_cpt(){
 }
 
 add_shortcode('cpt_download', 'create_shortcode_cpt');
+
+function create_shortcode_cpt_slide(){ ob_start();?>
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+<?php  echo ob_get_clean();}
+
+add_shortcode('cpt_slide', 'create_shortcode_cpt_slide');
